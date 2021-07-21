e33fcde5a526f2df33a35c394b2414eb497c2ddf
wolverine699 <himanshusachan91866@gmail.com>
2021-07-21 22:13:53 +0530

    init

---
+++ app/src/main/java/com/example/loginpage/replacement.java
@@ -0,0 +1,15 @@
+package com.example.loginpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class replacement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_replacement);
+
+    }
+}
